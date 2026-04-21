8b731da7cbe88b1226011a5798f9fa920d58e600
Sandeep Madineni <smadine@conviva.com>
2026-04-21 18:18:32 +0530

    Checksums for 4.0.33

--- Package.swift
+++ Package.swift
@@ -18,11 +18,11 @@ let package = Package(
         .binaryTarget(
             name: "ConvivaBrightcove",
             url: "https://github.com/Conviva/ConvivaBrightcove/releases/download/4.0.33/ConvivaBrightcove.xcframework.zip",
-            checksum: "bf5ea4227355ba54bdfe2bc528e4a793731e3127d46baca408606dd3abe4bf02"),
+            checksum: "008c8932490a67858844178eef026a05f2b8e9fb1e95995362e3a26e7174e335"),
         
         .binaryTarget(
                 name: "ConvivaSDK",
                 url: "https://github.com/Conviva/ConvivaSDK/releases/download/4.3.1/ConvivaSDK.xcframework.zip",
-                checksum: "008c8932490a67858844178eef026a05f2b8e9fb1e95995362e3a26e7174e335")
+                checksum: "0b6c61813c111a42243c85d9e70bf2e17ab68b9b2d0ed533d808adb75b810d88")
      ]
 )
